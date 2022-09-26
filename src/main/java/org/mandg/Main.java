ac550622161e598205a7207c88e79a7f23577162
Ravi Salunkhe <ravi.salunkhe@mandg.com>
2022-09-26 13:07:54 +0530

    second commit

--- src/main/java/org/mandg/Main.java
+++ src/main/java/org/mandg/Main.java
@@ -3,5 +3,9 @@ package org.mandg;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        System.out.println("sample added");
+
+
     }
 }
